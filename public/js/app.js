2590c0365cf00d588182a81f67094046bfb21ba8
Andy Osland <andy@andyosland.com>
2020-09-06 01:05:53 +0000

    adjusted for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -24,7 +24,7 @@ weatherForm.addEventListener('submit', (e) =>{
     
     const location = search.value
 
-    fetch('http://10.252.3.100:3000/Weather?address='+location).then((response) => {
+    fetch('/Weather?address='+location).then((response) => {
         response.json().then((data) => {
             if(data.error){
                 //console.log(data.error)
