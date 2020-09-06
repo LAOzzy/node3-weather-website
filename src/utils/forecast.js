63906981ed1a12c62b7f3e3b794df1b3fb2cf8a4
Andy Osland <andy@andyosland.com>
2020-09-06 01:29:37 +0000

    more logging

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,6 +4,7 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=7bfddb31e1b56dd605524cb9f1610de4&query='+ encodeURIComponent(longitude)+','+ encodeURIComponent(latitude) +'&units=f'
 
     request({url, json:true}, (error, {body} = {}) =>{ 
+        console.log(body)
         if(error){
             console.log(body.error.type+":"+body.error.info, undefined)
         } else{
